2665e44ad8646c1964c66e21eed23e90db7223b5
Tony Stark <ironman0x7b2@protonmail.com>
2024-08-09 20:37:57 +0530

    feat: add SyncSessionUsageWithBlockchain worker

--- workers/session.go
+++ workers/session.go
@@ -2,15 +2,17 @@ package workers
 
 import (
 	gocontext "context"
+	"errors"
 
 	sdkmath "cosmossdk.io/math"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/sentinel-official/dvpn-node/context"
 	"github.com/sentinel-official/dvpn-node/database/operations"
 )
 
-// handleSyncSessionUsage returns a function that synchronizes peer session usage statistics with the database.
-func handleSyncSessionUsage(ctx *context.Context) func() error {
+// handleSyncSessionUsageWithDB returns a function that synchronizes peer session usage statistics with the database.
+func handleSyncSessionUsageWithDB(ctx *context.Context) func() error {
 	return func() error {
 		// Retrieve peer statistics from the service.
 		statistics, err := ctx.Service().PeerStatistics(gocontext.TODO())
@@ -49,36 +51,64 @@ func handleSyncSessionUsage(ctx *context.Context) func() error {
 // handleValidateSessionUsage returns a function that validates session usage and removes peers if necessary.
 func handleValidateSessionUsage(ctx *context.Context) func() error {
 	return func() error {
-		// Retrieve sessions from the database
+		// Retrieve sessions from the database.
 		sessions, err := operations.SessionFind(ctx.DB(), nil)
 		if err != nil {
 			return err
 		}
 
-		// Iterate over each session
+		// Iterate over each session.
 		for _, s := range sessions {
-			// Determine if the peer should be removed
+			// Determine if the peer should be removed based on session limits.
 			removePeer := false
 
-			// Check if the session has exceeded its byte limit
+			// Check if the session has exceeded its byte limit.
 			if s.GetBytes().GTE(s.GetMaxBytes()) {
 				removePeer = true
 			}
 
-			// Check if the session has exceeded its duration limit
+			// Check if the session has exceeded its duration limit.
 			if s.GetDuration() >= s.GetMaxDuration() {
 				removePeer = true
 			}
 
-			// If the peer should not be removed, skip to the next session
+			// If the peer should not be removed, skip to the next session.
 			if !removePeer {
 				continue
 			}
 
-			// Attempt to remove the peer identified by session.PeerKey
+			// Attempt to remove the peer identified by session.PeerKey.
 			if err := ctx.RemovePeerIfExistsForKey(gocontext.TODO(), s.PeerKey); err != nil {
 				return err
 			}
+		}
+
+		return nil
+	}
+}
+
+// handleSyncSessionUsageWithBlockchain returns a function that syncs session usage with the blockchain.
+func handleSyncSessionUsageWithBlockchain(ctx *context.Context) func() error {
+	return func() error {
+		// Retrieve sessions from the database.
+		sessions, err := operations.SessionFind(ctx.DB(), nil)
+		if err != nil {
+			return err
+		}
+
+		// Prepare a slice of transaction messages for session updates.
+		var messages []sdk.Msg
+		for _, s := range sessions {
+			messages = append(messages, s.MsgUpdateSessionRequest())
+		}
+
+		// Broadcast the transaction messages to the blockchain.
+		res, err := ctx.BroadcastTx(gocontext.TODO(), messages...)
+		if err != nil {
+			return err
+		}
+		if !res.TxResult.IsOK() {
+			return errors.New(res.TxResult.GetLog())
 		}
 
 		return nil
